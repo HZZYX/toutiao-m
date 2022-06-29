111dbe276b6b84a2bb3b222e2311b721bb8991f6
何铸 <Morii-He@foxmail.com>
2022-06-29 20:19:18 +0800

    今天写到了首页的tabbar，登录那个位置还有一点点缺陷

--- src/main.js
+++ src/main.js
@@ -6,11 +6,28 @@ import Vant from 'vant'
 import 'vant/lib/index.css'
 import './style/index.less'
 import 'amfe-flexible'
+import '@/mock/mockServe.js'
 
 Vue.use(Vant)
 Vue.config.productionTip = false
+
+// 前置路由守卫
+router.beforeEach((to, from, next) => {
+  // from.name 来的时候的路由名字
+  // to.name   去的路由名字，当前路由的名字
+  const token = JSON.parse(window.localStorage.getItem('TOUTIAO_TOKEN'))
+  if (!token && to.name !== 'login') {
+    alert('请先登录')
+    next({
+      name: 'login'
+    })
+  } else {
+    next()
+  }
+})
+
 new Vue({
   router,
-  store,
+  store, // 挂载store，会在Vue实例对象上添加一个$store
   render: h => h(App)
 }).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import login from '@/store/login/login.js'
+// 需要使用插件一次
 Vue.use(Vuex)
 
+// 对外暴露Store类的一个实例
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    login
   }
 })
